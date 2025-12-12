214bb33f47d66689004a49325c21c832fc40b9d0
ghumman <ghummantech@gmail.com>
2025-12-12 15:51:22 -0600

    Update mission 5

--- launch-2-mission-5-who-lived.py
+++ launch-2-mission-5-who-lived.py
@@ -3,39 +3,35 @@ import runloop
 import motor_pair
 import motor
 
-
 vel = 180
 vel2 = 720
 
-startingAttachmentAngle = 70
+startingAttachmentAngle = 110
 
 async def main():
 
     # Bring attachment to starting position
     await motor.run_to_absolute_position(port.D, startingAttachmentAngle, vel)
 
-    # Move left
-    await motor.run_for_degrees(port.E, 200, vel)
-
-    # Move right
-    await motor.run_for_degrees(port.A, -220, vel)
-
-
     # Pair motors
     motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
 
-    # # Move forward
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 680, 0, velocity=vel)
+    # Move forward
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 870, 0, velocity=vel)
 
+    # Move attachment to left
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 100, -80, velocity=vel2)
 
-    # Make a right turn
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 130, 100, velocity=vel2)
+    # Add delay before going more left to add more force and settle things down
+    await runloop.sleep_ms(500)
 
-    # Make left turn
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 100, -100, velocity=vel2)
+    # Move attachment to left
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 100, -80, velocity=vel)
 
     # Return
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, -800, 0, velocity=vel)
+
+    # Stop motor pair
     motor_pair.stop(motor_pair.PAIR_1)
 
 runloop.run(main())
